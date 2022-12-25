0cdbdbf9806650f274bb6d6228407a7e555417ae
mariammahboub <110869557+mariammahboub@users.noreply.github.com>
2022-12-25 15:05:10 -0600

    Update Test.java

--- src/Controller/Test.java
+++ src/Controller/Test.java
@@ -1,17 +1,5 @@
 package Controller;
 
 public class Test {
-    private Test test;
 
-    public Test(Test test) {
-        this.test = test;
-    }
-
-    public Test getTest() {
-        return test;
-    }
-
-    public void setTest(Test test) {
-        this.test = test;
-    }
 }
